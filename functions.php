3dc2ef9a98ae91477fda455318992084544d7d5c
Code IT Hub <78258092+codeithub@users.noreply.github.com>
2021-07-10 16:01:45 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,10 @@
+add_filter( 'woocommerce_cart_item_price', 'codeithub_change_cart_table_price_display', 30, 3 );
+  
+function codeithub_change_cart_table_price_display( $price, $values, $cart_item_key ) {
+   $slashed_price = $values['data']->get_price_html();
+   $is_on_sale = $values['data']->is_on_sale();
+   if ( $is_on_sale ) {
+      $price = $slashed_price;
+   }
+   return $price;
+}
